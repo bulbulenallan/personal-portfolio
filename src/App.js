f1c8c3525bfe53d04b069abcd39cfa3b523bec23
Allan Kipchumba <bulbulen5606@gmail.com>
2023-09-16 01:14:11 +0300

    updated app.js to dispaly home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar'; // Correct the import statement to include 'Home'
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
-    </div>
+      <Home/>
+    </>
   );
 }
 
